0a95b579d3c5dc244cb7deaa268b7ee641216b41
Matheus Mastrocinque <tec.mastrocinque@gmail.com>
2021-10-17 14:28:14 -0300

    Adding new adjustments

---
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Animal } from '../animais';
+import { AnimaisService } from '../animais.service';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css']
+})
+export class DetalheAnimalComponent implements OnInit {
+
+  animalId!: number;
+  animal$!: Observable<Animal>;
+
+  constructor(
+    private animaisService: AnimaisService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorID(this.animalId); 
+  }
+
+}
---
+++ src/app/home/novo-usuario/usuario-senha-iguais.validator.ts
@@ -0,0 +1,13 @@
+import { FormGroup } from '@angular/forms';
+
+export function usuarioSenhaIguais(formGroup: FormGroup) {
+    const username = formGroup.get('userName')?.value ?? '';
+    const password = formGroup.get('password')?.value ?? '';
+
+    if (username.trim() + password.trim()) {
+        return username !== password ? null : { senhaIgualUsuario: true };
+    } else {
+        return null;
+    }
+
+}
