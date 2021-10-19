0c30a6c0f2efcb6f8ae9f5ead6214f32679d7568
Matheus Mastrocinque <tec.mastrocinque@gmail.com>
2021-10-18 23:20:06 -0300

    Adjustments

--- src/app/componentes/cabecalho/cabecalho.module.ts
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from '../menu/menu.module';
 
 
 
 @NgModule({
   declarations: [CabecalhoComponent],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, MenuModule
   ],
   exports: [CabecalhoComponent],
 })
